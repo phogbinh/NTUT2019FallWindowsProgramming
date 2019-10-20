91256cd0d08365f8c5e0d0307af303c9c28c0f07
phogbinh <phongbinh2511@gmail.com>
2019-10-20 16:15:17 +0800

    InitializeProductInfo

--- Homework 3/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Views/ReplenishmentForm.cs
+++ Homework 3/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Views/ReplenishmentForm.cs
@@ -6,6 +6,10 @@ namespace OrderAndStorageManagementSystem.Views
 {
     public partial class ReplenishmentForm : Form
     {
+        private const string PRODUCT_NAME_TEXT = "商品名稱： ";
+        private const string PRODUCT_TYPE_TEXT = "商品類別： ";
+        private const string PRODUCT_PRICE_TEXT = "商品單價： ";
+        private const string PRODUCT_STORAGE_QUANTITY_TEXT = "庫存數量： ";
         private Model _model;
         private Product _product;
 
@@ -14,6 +18,17 @@ namespace OrderAndStorageManagementSystem.Views
             InitializeComponent();
             _model = modelData;
             _product = productData;
+            // Initial UI States
+            InitializeProductInfo();
+        }
+
+        // Protest on Dr.Smell
+        private void InitializeProductInfo()
+        {
+            _productName.Text = PRODUCT_NAME_TEXT + _product.Name;
+            _productType.Text = PRODUCT_TYPE_TEXT + _product.Type;
+            _productPrice.Text = PRODUCT_PRICE_TEXT + _product.Price.GetStringWithCurrencyUnit(AppDefinition.TAIWAN_CURRENCY_UNIT);
+            _productStorageQuantity.Text = PRODUCT_STORAGE_QUANTITY_TEXT + _product.StorageQuantity;
         }
     }
 }
