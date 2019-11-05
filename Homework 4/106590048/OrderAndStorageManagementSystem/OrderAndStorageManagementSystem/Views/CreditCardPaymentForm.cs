ca3af32d9b713993d25be7e63f9e4c1aa14c1a40
kyo <phongbinh2511@gmail.com>
2019-11-05 19:01:15 +0800

    Added input inspectors for input inspecting textboxes and drop-down lists. Apart from this, the error provider view is now updated on every input inspectors collection changed.

--- Homework 4/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Views/CreditCardPaymentForm.cs
+++ Homework 4/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Views/CreditCardPaymentForm.cs
@@ -1,6 +1,9 @@
-﻿using OrderAndStorageManagementSystem.Models;
+﻿using InputInspectingElements;
+using InputInspectingElements.InputInspectingControls;
+using OrderAndStorageManagementSystem.Models;
 using OrderAndStorageManagementSystem.Views.Utilities;
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace OrderAndStorageManagementSystem.Views
@@ -10,6 +13,8 @@ namespace OrderAndStorageManagementSystem.Views
         private const string ORDER_COMPLETE_TITLE = "訂購狀態";
         private const string ORDER_COMPLETE_MESSAGE = "訂購完成";
         private Model _model;
+        private List<InputInspectingTextBox> _textBoxes;
+        private List<InputInspectingDropDownList> _dropDownLists;
 
         public CreditCardPaymentForm(Model modelData)
         {
@@ -19,6 +24,14 @@ namespace OrderAndStorageManagementSystem.Views
             this.FormClosed += (sender, eventArguments) => _cardSecurityCodeField.Text = AppDefinition.EMPTY_STRING;
             _submitButton.Click += ClickSubmitButton;
             InitializeInputLimits();
+            // Input inspecting textboxes
+            InitializeInputInspectingTextBoxesTextBoxInspectors();
+            InitializeInputInspectingTextBoxes();
+            InitializeInputInspectingTextBoxesTextBoxInspectorsCollectionChangedEventHandlers();
+            // Input inspecting drop-down lists
+            InitializeInputInspectingDropDownListsDropDownListInspectors();
+            InitializeInputInspectingDropDownLists();
+            InitializeInputInspectingDropDownListsDropDownListInspectorsCollectionChangedEventHandlers();
         }
 
         /// <summary>
@@ -45,6 +58,88 @@ namespace OrderAndStorageManagementSystem.Views
             _cardNumberThirdField.KeyPress += InputHelper.InputNumbersOrBackSpace;
             _cardNumberFourthField.KeyPress += InputHelper.InputNumbersOrBackSpace;
             _cardSecurityCodeField.KeyPress += InputHelper.InputNumbersOrBackSpace;
+        }
+
+        /// <summary>
+        /// Initialize textbox inspectors for input inspecting textboxes.
+        /// </summary>
+        private void InitializeInputInspectingTextBoxesTextBoxInspectors()
+        {
+            _lastNameField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY);
+            _firstNameField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY);
+            _cardNumberFirstField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY | InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_OF_FULL_LENGTH);
+            _cardNumberSecondField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY | InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_OF_FULL_LENGTH);
+            _cardNumberThirdField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY | InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_OF_FULL_LENGTH);
+            _cardNumberFourthField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY | InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_OF_FULL_LENGTH);
+            _cardSecurityCodeField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY | InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_OF_FULL_LENGTH);
+            _mailField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY | InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_MAIL);
+            _addressField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY);
+        }
+
+        /// <summary>
+        /// Initialize the input inspecting textboxes _textBoxes.
+        /// </summary>
+        private void InitializeInputInspectingTextBoxes()
+        {
+            _textBoxes = new List<InputInspectingTextBox>();
+            _textBoxes.Add(_lastNameField);
+            _textBoxes.Add(_firstNameField);
+            _textBoxes.Add(_cardNumberFirstField);
+            _textBoxes.Add(_cardNumberSecondField);
+            _textBoxes.Add(_cardNumberThirdField);
+            _textBoxes.Add(_cardNumberFourthField);
+            _textBoxes.Add(_cardSecurityCodeField);
+            _textBoxes.Add(_mailField);
+            _textBoxes.Add(_addressField);
+        }
+
+        /// <summary>
+        /// Initialize the handlers for the event TextBoxInspectorsCollectionChanged of input inspecting textboxes.
+        /// </summary>
+        private void InitializeInputInspectingTextBoxesTextBoxInspectorsCollectionChangedEventHandlers()
+        {
+            foreach ( InputInspectingTextBox textBox in _textBoxes )
+            {
+                textBox.TextBoxInspectorsCollectionChanged += () => UpdateErrorProviderView(textBox, textBox.GetInputInspectorsError());
+            }
+        }
+
+        /// <summary>
+        /// Update the view of the error provider.
+        /// </summary>
+        private void UpdateErrorProviderView(Control control, string controlInputInspectorsError)
+        {
+            _errorProvider.SetError(control, controlInputInspectorsError);
+        }
+
+        /// <summary>
+        /// Initialize drop-down list inspectors for input inspecting drop-down lists.
+        /// </summary>
+        private void InitializeInputInspectingDropDownListsDropDownListInspectors()
+        {
+            _cardDateMonthField.AddDropDownListInspectors(InputInspectorTypeHelper.FLAG_DROP_DOWN_LIST_IS_SELECTED);
+            _cardDateYearField.AddDropDownListInspectors(InputInspectorTypeHelper.FLAG_DROP_DOWN_LIST_IS_SELECTED);
+        }
+
+        /// <summary>
+        /// Initialize the input inspecting drop-down lists _dropDownLists.
+        /// </summary>
+        private void InitializeInputInspectingDropDownLists()
+        {
+            _dropDownLists = new List<InputInspectingDropDownList>();
+            _dropDownLists.Add(_cardDateMonthField);
+            _dropDownLists.Add(_cardDateYearField);
+        }
+
+        /// <summary>
+        /// Initialize the handlers for the event DropDownListInspectorsCollectionChanged of input inspecting drop-down lists.
+        /// </summary>
+        private void InitializeInputInspectingDropDownListsDropDownListInspectorsCollectionChangedEventHandlers()
+        {
+            foreach ( InputInspectingDropDownList dropDownList in _dropDownLists )
+            {
+                dropDownList.DropDownListInspectorsCollectionChanged += () => UpdateErrorProviderView(dropDownList, dropDownList.GetInputInspectorsError());
+            }
         }
     }
 }
