07f8936ab6a520f71da592c7b1d5f6c3c978d884
kyo <phongbinh2511@gmail.com>
2019-11-07 18:58:49 +0800

    Checked whether the argument oneBinaryNumberOnFlag is an one-bit-on-flag.

--- Homework 4/106590048/OrderAndStorageManagementSystem/InputInspectingElements/BinaryNumberHelper.cs
+++ Homework 4/106590048/OrderAndStorageManagementSystem/InputInspectingElements/BinaryNumberHelper.cs
@@ -1,12 +1,20 @@
-﻿namespace InputInspectingElements
+﻿using System;
+
+namespace InputInspectingElements
 {
     public static class BinaryNumberHelper
     {
+        private const string ERROR_INVALID_ONE_BINARY_NUMBER_ON_FLAG = "The given flag is not an one-bit-on-flag.";
+
         /// <summary>
         /// Return true if the given flag has the-one-and-only-on-bit of oneBitOnFlag on.
         /// </summary>
         public static bool IsContainingOneBinaryNumberOnFlag(int flag, int oneBinaryNumberOnFlag)
         {
+            if ( !IsOneBinaryNumberOnFlag(oneBinaryNumberOnFlag) )
+            {
+                throw new ArgumentException(ERROR_INVALID_ONE_BINARY_NUMBER_ON_FLAG);
+            }
             return ( flag & oneBinaryNumberOnFlag ) == oneBinaryNumberOnFlag;
         }
 
