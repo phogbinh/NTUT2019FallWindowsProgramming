2d5ea2b82505dbb51017a9db8546088343206173
kyo <phongbinh2511@gmail.com>
2019-11-09 22:23:47 +0800

    InitializeProductsListBox().

--- Homework 4/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Views/ProductManagementForm.cs
+++ Homework 4/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Views/ProductManagementForm.cs
@@ -1,5 +1,7 @@
 ﻿using OrderAndStorageManagementSystem.Models;
+using OrderAndStorageManagementSystem.Models.Utilities;
 using OrderAndStorageManagementSystem.PresentationModels;
+using OrderAndStorageManagementSystem.Views.Utilities;
 using System.Windows.Forms;
 
 namespace OrderAndStorageManagementSystem.Views
@@ -14,6 +16,19 @@ namespace OrderAndStorageManagementSystem.Views
             InitializeComponent();
             _productManagementPresentationModel = productManagementPresentationModelData;
             _model = modelData;
+            // Initial UI States
+            InitializeProductsListBox();
+        }
+
+        /// <summary>
+        /// Initialize _productsListBox.
+        /// </summary>
+        private void InitializeProductsListBox()
+        {
+            foreach ( Product product in _model.Products )
+            {
+                _productsListBox.Items.Add(new ProductsListBoxItem(product));
+            }
         }
     }
 }
---
+++ Homework 4/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Views/Utilities/ProductsListBoxItem.cs
@@ -0,0 +1,22 @@
+﻿using OrderAndStorageManagementSystem.Models.Utilities;
+
+namespace OrderAndStorageManagementSystem.Views.Utilities
+{
+    public class ProductsListBoxItem
+    {
+        private Product _product;
+
+        public ProductsListBoxItem(Product productData)
+        {
+            _product = productData;
+        }
+
+        /// <summary>
+        /// Return the name of the product.
+        /// </summary>
+        public override string ToString()
+        {
+            return _product.Name;
+        }
+    }
+}
