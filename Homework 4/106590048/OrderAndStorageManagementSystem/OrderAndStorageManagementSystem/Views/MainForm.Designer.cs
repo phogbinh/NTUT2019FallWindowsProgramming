58a63daf95659513b1517d1661d767d6c432ffd4
kyo <phongbinh2511@gmail.com>
2019-11-09 20:54:42 +0800

    Created Product Manage System button.

--- Homework 4/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Views/MainForm.Designer.cs
+++ Homework 4/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Views/MainForm.Designer.cs
@@ -33,6 +33,7 @@
             this._inventorySystemButton = new System.Windows.Forms.Button();
             this._lastRowLayout = new System.Windows.Forms.TableLayoutPanel();
             this._exitButton = new System.Windows.Forms.Button();
+            this._productManageSystemButton = new System.Windows.Forms.Button();
             this._mainFormLayout.SuspendLayout();
             this._lastRowLayout.SuspendLayout();
             this.SuspendLayout();
@@ -41,26 +42,28 @@
             // 
             this._mainFormLayout.ColumnCount = 1;
             this._mainFormLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this._mainFormLayout.Controls.Add(this._lastRowLayout, 0, 3);
             this._mainFormLayout.Controls.Add(this._orderSystemButton, 0, 0);
             this._mainFormLayout.Controls.Add(this._inventorySystemButton, 0, 1);
-            this._mainFormLayout.Controls.Add(this._lastRowLayout, 0, 2);
+            this._mainFormLayout.Controls.Add(this._productManageSystemButton, 0, 2);
             this._mainFormLayout.Dock = System.Windows.Forms.DockStyle.Fill;
             this._mainFormLayout.Location = new System.Drawing.Point(0, 0);
             this._mainFormLayout.Name = "_mainFormLayout";
-            this._mainFormLayout.RowCount = 3;
-            this._mainFormLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this._mainFormLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this._mainFormLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this._mainFormLayout.RowCount = 4;
+            this._mainFormLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this._mainFormLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this._mainFormLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this._mainFormLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this._mainFormLayout.Size = new System.Drawing.Size(800, 450);
             this._mainFormLayout.TabIndex = 0;
             // 
             // _orderSystemButton
             // 
             this._orderSystemButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this._orderSystemButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( ( byte )( 136 ) ));
+            this._orderSystemButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this._orderSystemButton.Location = new System.Drawing.Point(3, 3);
             this._orderSystemButton.Name = "_orderSystemButton";
-            this._orderSystemButton.Size = new System.Drawing.Size(794, 143);
+            this._orderSystemButton.Size = new System.Drawing.Size(794, 106);
             this._orderSystemButton.TabIndex = 0;
             this._orderSystemButton.Text = "Order System";
             this._orderSystemButton.UseVisualStyleBackColor = true;
@@ -68,10 +71,10 @@
             // _inventorySystemButton
             // 
             this._inventorySystemButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this._inventorySystemButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( ( byte )( 136 ) ));
-            this._inventorySystemButton.Location = new System.Drawing.Point(3, 152);
+            this._inventorySystemButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this._inventorySystemButton.Location = new System.Drawing.Point(3, 115);
             this._inventorySystemButton.Name = "_inventorySystemButton";
-            this._inventorySystemButton.Size = new System.Drawing.Size(794, 143);
+            this._inventorySystemButton.Size = new System.Drawing.Size(794, 106);
             this._inventorySystemButton.TabIndex = 1;
             this._inventorySystemButton.Text = "Inventory System";
             this._inventorySystemButton.UseVisualStyleBackColor = true;
@@ -83,24 +86,35 @@
             this._lastRowLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this._lastRowLayout.Controls.Add(this._exitButton, 1, 0);
             this._lastRowLayout.Dock = System.Windows.Forms.DockStyle.Fill;
-            this._lastRowLayout.Location = new System.Drawing.Point(3, 301);
+            this._lastRowLayout.Location = new System.Drawing.Point(3, 339);
             this._lastRowLayout.Name = "_lastRowLayout";
             this._lastRowLayout.RowCount = 1;
             this._lastRowLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this._lastRowLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 74F));
-            this._lastRowLayout.Size = new System.Drawing.Size(794, 146);
-            this._lastRowLayout.TabIndex = 2;
+            this._lastRowLayout.Size = new System.Drawing.Size(794, 108);
+            this._lastRowLayout.TabIndex = 3;
             // 
             // _exitButton
             // 
             this._exitButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this._exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( ( byte )( 136 ) ));
+            this._exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this._exitButton.Location = new System.Drawing.Point(400, 3);
             this._exitButton.Name = "_exitButton";
-            this._exitButton.Size = new System.Drawing.Size(391, 140);
+            this._exitButton.Size = new System.Drawing.Size(391, 102);
             this._exitButton.TabIndex = 0;
             this._exitButton.Text = "Exit";
             this._exitButton.UseVisualStyleBackColor = true;
+            // 
+            // _productManageSystemButton
+            // 
+            this._productManageSystemButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._productManageSystemButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this._productManageSystemButton.Location = new System.Drawing.Point(3, 227);
+            this._productManageSystemButton.Name = "_productManageSystemButton";
+            this._productManageSystemButton.Size = new System.Drawing.Size(794, 106);
+            this._productManageSystemButton.TabIndex = 4;
+            this._productManageSystemButton.Text = "Product Manage System";
+            this._productManageSystemButton.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -123,5 +137,6 @@
         private System.Windows.Forms.Button _inventorySystemButton;
         private System.Windows.Forms.TableLayoutPanel _lastRowLayout;
         private System.Windows.Forms.Button _exitButton;
+        private System.Windows.Forms.Button _productManageSystemButton;
     }
 }
