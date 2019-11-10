3bc29d1e1a17496038e36684ea6b4d50e97988cd
kyo <phongbinh2511@gmail.com>
2019-11-10 16:34:46 +0800

    Initialized input inspectors for all input inspecting textboxes and drop-down lists.

--- Homework 4/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Views/ProductManagementForm.cs
+++ Homework 4/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Views/ProductManagementForm.cs
@@ -1,9 +1,12 @@
 ﻿using InputInspectingElements;
+using InputInspectingElements.InputInspectingControls;
+using InputInspectingElements.InputInspectorsCollections;
 using OrderAndStorageManagementSystem.Models;
 using OrderAndStorageManagementSystem.Models.Utilities;
 using OrderAndStorageManagementSystem.PresentationModels;
 using OrderAndStorageManagementSystem.Views.Utilities;
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace OrderAndStorageManagementSystem.Views
@@ -13,6 +16,9 @@ namespace OrderAndStorageManagementSystem.Views
         private const string ERROR_NULL_CURRENT_SELECTED_PRODUCT = "The current selected product is null.";
         private ProductManagementPresentationModel _productManagementPresentationModel;
         private Model _model;
+        private List<InputInspectingTextBox> _textBoxes;
+        private List<InputInspectingDropDownList> _dropDownLists;
+        private InputInspectorsCollection _inputInspectorsCollection;
 
         public ProductManagementForm(ProductManagementPresentationModel productManagementPresentationModelData, Model modelData)
         {
@@ -25,10 +31,18 @@ namespace OrderAndStorageManagementSystem.Views
             _productManagementPresentationModel.SaveButtonEnabledChanged += UpdateSaveButtonView;
             // UI
             _productsListBox.SelectedIndexChanged += (sender, eventArguments) => _productManagementPresentationModel.SetCurrentSelectedProduct(( ( ProductsListBoxItem )_productsListBox.SelectedItem ).Product);
-            _productNameField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY);
-            _productNameField.TextBoxInspectorsCollectionChanged += () => UpdateErrorProviderView(_productNameField, _productNameField.GetInputInspectorsError());
             _productPriceField.KeyPress += InputHelper.InputNumbersOrBackSpace;
             _productImageBrowseButton.Click += (sender, eventArguments) => BrowseImageAndSetProductImagePath();
+            // Input inspecting textboxes
+            InitializeInputInspectingTextBoxesTextBoxInspectors();
+            InitializeInputInspectingTextBoxes();
+            InitializeInputInspectingTextBoxesTextBoxInspectorsCollectionChangedEventHandlers();
+            // Input inspecting drop-down lists
+            InitializeInputInspectingDropDownListsDropDownListInspectors();
+            InitializeInputInspectingDropDownLists();
+            InitializeInputInspectingDropDownListsDropDownListInspectorsCollectionChangedEventHandlers();
+            // Input inspectors collection
+            InitializeInputInspectorsCollection();
             // Initial UI States
             InitializeProductTypeField();
             InitializeProductsListBox();
@@ -85,6 +99,82 @@ namespace OrderAndStorageManagementSystem.Views
             if ( dialog.ShowDialog() == DialogResult.OK )
             {
                 _productImagePathField.Text = dialog.FileName;
+            }
+        }
+
+        /// <summary>
+        /// Initialize textbox inspectors for input inspecting textboxes.
+        /// </summary>
+        private void InitializeInputInspectingTextBoxesTextBoxInspectors()
+        {
+            _productNameField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY);
+            _productPriceField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY);
+            _productImagePathField.AddTextBoxInspectors(InputInspectorTypeHelper.FLAG_TEXT_BOX_IS_NOT_EMPTY);
+        }
+
+        /// <summary>
+        /// Initialize the input inspecting textboxes _textBoxes.
+        /// </summary>
+        private void InitializeInputInspectingTextBoxes()
+        {
+            _textBoxes = new List<InputInspectingTextBox>();
+            _textBoxes.Add(_productNameField);
+            _textBoxes.Add(_productPriceField);
+            _textBoxes.Add(_productImagePathField);
+        }
+
+        /// <summary>
+        /// Initialize the handlers for the event TextBoxInspectorsCollectionChanged of input inspecting textboxes.
+        /// </summary>
+        private void InitializeInputInspectingTextBoxesTextBoxInspectorsCollectionChangedEventHandlers()
+        {
+            foreach ( InputInspectingTextBox textBox in _textBoxes )
+            {
+                textBox.TextBoxInspectorsCollectionChanged += () => UpdateErrorProviderView(textBox, textBox.GetInputInspectorsError());
+            }
+        }
+
+        /// <summary>
+        /// Initialize drop-down list inspectors for input inspecting drop-down lists.
+        /// </summary>
+        private void InitializeInputInspectingDropDownListsDropDownListInspectors()
+        {
+            _productTypeField.AddDropDownListInspectors(InputInspectorTypeHelper.FLAG_DROP_DOWN_LIST_IS_SELECTED);
+        }
+
+        /// <summary>
+        /// Initialize the input inspecting drop-down lists _dropDownLists.
+        /// </summary>
+        private void InitializeInputInspectingDropDownLists()
+        {
+            _dropDownLists = new List<InputInspectingDropDownList>();
+            _dropDownLists.Add(_productTypeField);
+        }
+
+        /// <summary>
+        /// Initialize the handlers for the event DropDownListInspectorsCollectionChanged of input inspecting drop-down lists.
+        /// </summary>
+        private void InitializeInputInspectingDropDownListsDropDownListInspectorsCollectionChangedEventHandlers()
+        {
+            foreach ( InputInspectingDropDownList dropDownList in _dropDownLists )
+            {
+                dropDownList.DropDownListInspectorsCollectionChanged += () => UpdateErrorProviderView(dropDownList, dropDownList.GetInputInspectorsError());
+            }
+        }
+
+        /// <summary>
+        /// Initialize the input inspectors collection.
+        /// </summary>
+        private void InitializeInputInspectorsCollection()
+        {
+            _inputInspectorsCollection = new InputInspectorsCollection();
+            foreach ( InputInspectingTextBox textBox in _textBoxes )
+            {
+                _inputInspectorsCollection.AddInputInspectorsList(textBox.GetInputInspectors());
+            }
+            foreach ( InputInspectingDropDownList dropDownList in _dropDownLists )
+            {
+                _inputInspectorsCollection.AddInputInspectorsList(dropDownList.GetInputInspectors());
             }
         }
 
