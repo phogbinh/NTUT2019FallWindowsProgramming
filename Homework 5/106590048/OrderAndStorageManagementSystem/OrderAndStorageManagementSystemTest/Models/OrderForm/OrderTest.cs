face5fc93aab26833bc209a2de3f3a0d3f664ba4
kyo <phongbinh2511@gmail.com>
2019-11-12 20:18:17 +0800

    Formatted OrderTest.

--- Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystemTest/Models/OrderForm/OrderTest.cs
+++ Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystemTest/Models/OrderForm/OrderTest.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OrderAndStorageManagementSystem.Models.OrderForm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OrderAndStorageManagementSystem.Models.OrderForm.Test
 {
