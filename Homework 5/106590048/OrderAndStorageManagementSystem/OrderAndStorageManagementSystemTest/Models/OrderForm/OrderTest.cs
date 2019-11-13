53fa8515b76faeb42fb7356eb620c3e020c0e44d
kyo <phongbinh2511@gmail.com>
2019-11-13 11:51:22 +0800

    Added implementation for TestGetOrderItemsCount().

--- Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystemTest/Models/OrderForm/OrderTest.cs
+++ Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystemTest/Models/OrderForm/OrderTest.cs
@@ -128,7 +128,13 @@ namespace OrderAndStorageManagementSystem.Models.OrderForm.Test
         [TestMethod()]
         public void TestGetOrderItemsCount()
         {
-            Assert.Fail();
+            for ( int i = 0; i < 10; i++ )
+            {
+                Product product = new Product(i, "product name", "product type", new Money(i), i, "product description", "product image path");
+                OrderItem orderItem = new OrderItem(product);
+                _orderItems.Add(orderItem);
+            }
+            Assert.AreEqual(_order.GetOrderItemsCount(), 10);
         }
 
         [TestMethod()]
