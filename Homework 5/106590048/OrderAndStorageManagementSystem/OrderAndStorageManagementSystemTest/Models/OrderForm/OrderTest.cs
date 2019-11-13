7ae53a42b70e66899bcfd7833a44517df94a44af
kyo <phongbinh2511@gmail.com>
2019-11-13 19:55:10 +0800

    Implemented DUMP_INTEGER and DUMP_STRING onto the function TestSetOrderItemQuantity().

--- Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystemTest/Models/OrderForm/OrderTest.cs
+++ Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystemTest/Models/OrderForm/OrderTest.cs
@@ -240,7 +240,7 @@ namespace OrderAndStorageManagementSystem.Models.OrderForm.Test
         {
             for ( int i = 0; i < 10; i++ )
             {
-                Product product = new Product(i, "product name", "product type", new Money(i), i, "product description", "product image path");
+                Product product = new Product(DUMP_INTEGER, DUMP_STRING, DUMP_STRING, new Money(DUMP_INTEGER), DUMP_INTEGER, DUMP_STRING, DUMP_STRING);
                 OrderItem orderItem = new OrderItem(product);
                 orderItem.OrderQuantity = 3;
                 _orderItems.Add(orderItem);
