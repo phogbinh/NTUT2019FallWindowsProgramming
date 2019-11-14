fdf75f4bb17ded46fa866a06ea8530ac0b63a5af
kyo <phongbinh2511@gmail.com>
2019-11-14 19:51:50 +0800

    Added OrderItemTest.

---
+++ Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystemTest/Models/OrderForm/OrderItemTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderAndStorageManagementSystem.Models.OrderForm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderAndStorageManagementSystem.Models.OrderForm.Test
+{
+    [TestClass()]
+    public class OrderItemTest
+    {
+        [TestMethod()]
+        public void TestOrderItem()
+        {
+            Assert.Fail();
+        }
+    }
+}
