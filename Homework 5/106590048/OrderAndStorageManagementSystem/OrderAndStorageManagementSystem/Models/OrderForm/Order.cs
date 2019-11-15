e15e594b3b52f57441190b4c5c9052d38ce6060e
kyo <phongbinh2511@gmail.com>
2019-11-15 22:40:34 +0800

    Modified the exception type to be more precise.

--- Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Models/OrderForm/Order.cs
+++ Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Models/OrderForm/Order.cs
@@ -208,7 +208,7 @@ namespace OrderAndStorageManagementSystem.Models.OrderForm
         {
             if ( !IsInOrderItemsIndexRange(orderItemIndex) )
             {
-                throw new ArgumentException(ERROR_ORDER_ITEM_INDEX_IS_OUT_OF_RANGE);
+                throw new ArgumentOutOfRangeException(ERROR_ORDER_ITEM_INDEX_IS_OUT_OF_RANGE);
             }
             return quantity > _orderItems[ orderItemIndex ].StorageQuantity;
         }
