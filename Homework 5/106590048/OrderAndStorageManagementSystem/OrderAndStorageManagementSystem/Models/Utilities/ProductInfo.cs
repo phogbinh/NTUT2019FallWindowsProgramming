4151b2fb4a9b54d3115e120e67cbd0592e2b7388
kyo <phongbinh2511@gmail.com>
2019-11-15 21:30:55 +0800

    Main task: Create the ProductInfo class which includes the product's name, type, price, description and image path. Sub-task: Added the class ProductInfo.

---
+++ Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Models/Utilities/ProductInfo.cs
@@ -0,0 +1,75 @@
+﻿namespace OrderAndStorageManagementSystem.Models.Utilities
+{
+    public class ProductInfo
+    {
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+            }
+        }
+        public Money Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                _price = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                _description = value;
+            }
+        }
+        public string ImagePath
+        {
+            get
+            {
+                return _imagePath;
+            }
+            set
+            {
+                _imagePath = value;
+            }
+        }
+        private string _name;
+        private string _type;
+        private Money _price;
+        private string _description;
+        private string _imagePath;
+
+        public ProductInfo(string nameData, string typeData, string priceData, string descriptionData, string imagePathData)
+        {
+            this.Name = nameData;
+            this.Type = typeData;
+            this.Price = new Money(int.Parse(priceData));
+            this.Description = descriptionData;
+            this.ImagePath = imagePathData;
+        }
+    }
+}
