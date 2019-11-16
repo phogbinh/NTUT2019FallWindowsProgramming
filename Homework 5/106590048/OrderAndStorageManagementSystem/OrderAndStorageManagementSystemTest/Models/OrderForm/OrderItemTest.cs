15bcaf36906ac4a16824da3415775bccc1a20b2f
kyo <phongbinh2511@gmail.com>
2019-11-16 19:38:05 +0800

    TestSetPropertyOrderQuantity().

--- Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystemTest/Models/OrderForm/OrderItemTest.cs
+++ Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystemTest/Models/OrderForm/OrderItemTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OrderAndStorageManagementSystem.Models.Utilities;
 using OrderAndStorageManagementSystemTest;
+using System;
 
 namespace OrderAndStorageManagementSystem.Models.OrderForm.Test
 {
@@ -17,6 +18,19 @@ namespace OrderAndStorageManagementSystem.Models.OrderForm.Test
         {
             _orderItem = new OrderItem(new Product(TestDefinition.DUMP_INTEGER, TestDefinition.DUMP_STRING, TestDefinition.DUMP_STRING, new Money(TestDefinition.DUMP_INTEGER), TestDefinition.DUMP_INTEGER, TestDefinition.DUMP_STRING, TestDefinition.DUMP_STRING));
             _target = new PrivateObject(_orderItem);
+        }
+
+
+        /// <summary>Tests the set property order quantity.</summary>
+        [TestMethod()]
+        public void TestSetPropertyOrderQuantity()
+        {
+            const string MEMBER_VARIBLE_NAME_ORDER_QUANTITY = "_orderQuantity";
+            Assert.ThrowsException<ArgumentException>(() => _orderItem.OrderQuantity = -1);
+            _orderItem.OrderQuantity = 0;
+            Assert.AreEqual(_target.GetFieldOrProperty(MEMBER_VARIBLE_NAME_ORDER_QUANTITY), 0);
+            _orderItem.OrderQuantity = 1998;
+            Assert.AreEqual(_target.GetFieldOrProperty(MEMBER_VARIBLE_NAME_ORDER_QUANTITY), 1998);
         }
 
         /// <summary>Tests the order item.</summary>
