3c49d1a49574a11cb1293300f2ad81a1ecd987e9
kyo <phongbinh2511@gmail.com>
2019-11-16 14:33:07 +0800

    Removed the now redundant ERROR_VALUE_IS_NEGATIVE.

--- Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Models/Utilities/Money.cs
+++ Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystem/Models/Utilities/Money.cs
@@ -6,17 +6,12 @@ namespace OrderAndStorageManagementSystem.Models.Utilities
 {
     public class Money
     {
-        private const string ERROR_VALUE_IS_NEGATIVE = "The given value is negative.";
         private const string NEGATIVE_SIGN = "-";
         private const int THOUSANDS_COUNT = 3;
         private int _value;
 
         public Money(int valueData)
         {
-            if ( valueData < 0 )
-            {
-                throw new ArgumentException(ERROR_VALUE_IS_NEGATIVE);
-            }
             _value = valueData;
         }
 
