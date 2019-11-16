d059a8c496bb09c724a7d1c1b6310c9833627974
kyo <phongbinh2511@gmail.com>
2019-11-16 20:59:31 +0800

    TestGetLineValues().

--- Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystemTest/Models/Utilities/DataBaseManagerTest.cs
+++ Homework 5/106590048/OrderAndStorageManagementSystem/OrderAndStorageManagementSystemTest/Models/Utilities/DataBaseManagerTest.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderAndStorageManagementSystemTest;
 using OrderAndStorageManagementSystemTest.Properties;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 namespace OrderAndStorageManagementSystem.Models.Utilities.Test
 {
     [TestClass()]
     public class DataBaseManagerTest
     {
+        private PrivateType _target;
+
+        /// <summary>Initializes this instance.</summary>
+        [TestInitialize()]
+        [DeploymentItem(TestDefinition.DEPLOYMENT_ITEM_FILE_PATH)]
+        public void Initialize()
+        {
+            _target = new PrivateType(typeof(DataBaseManager));
+        }
+
         /// <summary>Tests the get products from file.</summary>
         [TestMethod()]
         public void TestGetProductsFromFile()
@@ -30,6 +43,28 @@ namespace OrderAndStorageManagementSystem.Models.Utilities.Test
             Assert.AreEqual(expectedProducts[ 2 ].Price.GetString(), "7999");
             Assert.AreEqual(expectedProducts[ 2 ].StorageQuantity, 100);
             Assert.AreEqual(expectedProducts[ 2 ].Description, "Third product description.");
+        }
+
+        /// <summary>Tests the get line values.</summary>
+        [TestMethod()]
+        public void TestGetLineValues()
+        {
+            const string MEMBER_FUNCTION_NAME_GET_LINE_VALUES = "GetLineValues";
+            var arguments = new object[] { "0,1,2,3,4" };
+            string[] expectedLineValues = ( string[] )_target.InvokeStatic(MEMBER_FUNCTION_NAME_GET_LINE_VALUES, arguments);
+            for ( int index = 0; index < 5; index++ )
+            {
+                Assert.AreEqual(expectedLineValues[ index ], index.ToString());
+            }
+            arguments = new object[] { ",,,,,," };
+            expectedLineValues = ( string[] )_target.InvokeStatic(MEMBER_FUNCTION_NAME_GET_LINE_VALUES, arguments);
+            for ( int index = 0; index < 7; index++ )
+            {
+                Assert.AreEqual(expectedLineValues[ index ], "");
+            }
+            arguments = new object[] { "corrupted, length" };
+            TargetInvocationException expectedException = Assert.ThrowsException<TargetInvocationException>(() => _target.InvokeStatic(MEMBER_FUNCTION_NAME_GET_LINE_VALUES, arguments));
+            Assert.IsInstanceOfType(expectedException.InnerException, typeof(IOException));
         }
     }
 }
